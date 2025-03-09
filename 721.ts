0b8f01cd3f2e4b7019c58780c2c0cb7ba70d188c
Y0sHi <anthony@mail.com>
2025-03-09 17:32:46 -0500

    New best 31/50

--- 721.ts
+++ 721.ts
@@ -10,30 +10,80 @@ function accountsMerge(accounts: string[][]): string[][] {
      */
     accounts.forEach( (account: string[]) => {
         let [is_new, _key] = verify_account(account, all_accounts)
-        console.log("New name?: ", is_new, _key)
 
         if (!is_new) {
-            console.log("Updating existing <Record>...")
             // Merge Emails into existing account record
             all_accounts[_key] = [...all_accounts[_key], ...account.slice(1)]
         }
         else {
-            console.log(`Adding a new <Record>: ${account[0]}`)
             // Initialize a new entry
             all_accounts[_key] = account.slice(1)
         }
     })
 
-    console.log(" -Success- all_accounts: ", all_accounts)
-
-    return compile_answer(all_accounts) // You are here
+    return compile_answer(final_pass(all_accounts))
 };
+
+/**
+ * Recursive function 
+ */
+function final_pass(all_accounts: Record<string, string[]>) : Record<string, string[]> {
+    
+    // There's no need to apply this function anymore
+    if (Object.keys(all_accounts).length === 1) {
+        return all_accounts
+    }
+
+    let new_records = {}            // Updated account records
+    let updated_ids = []            // List of ids to avoid reiterating
+    let found_duplicates = false    // Discovered accounts with a matching email
+
+    // Compare every index of our object to each other
+    Object.entries(all_accounts).forEach( (account_i, i) => {
+        let emails_i = account_i[1]
+        if (updated_ids.includes(account_i[0])) { /* accounted for, continue */ }
+        else {
+            Object.entries(all_accounts).forEach( (account_j, j) => {
+                console.log("Scanned IDS: ", updated_ids)
+                if (i === j) { /* same entry, continue */ }
+                else {
+                    let emails_j = account_j[1]
+                    let new_emails = []
+
+                    // Check for duplicates
+                    if (emails_i.some( email => emails_j.includes(email))) {
+                        found_duplicates = true
+
+                        // Account has been accounted for
+                        updated_ids.push(account_j[0])
+
+                        new_emails = [...emails_i, ...emails_j] // Merge
+
+                        // Update records
+                        new_records[account_i[0]] = new_emails
+
+                    } else {
+                        // No duplicate were found
+                        new_records[account_i[0]] = account_i[1]
+                    }
+                }
+            })
+        }
+    })
+    
+    if (found_duplicates) {
+        console.log("Final Pass Detected a Duplicate...\nNew Records:", new_records)
+        new_records = final_pass(new_records)
+    }
+    console.log("Returning new records: ", new_records)
+    return new_records
+}
 
 /**
  * Remove duplicates, sort, and aggregate.
  */
 function compile_answer(all_accounts: Record<string, string[]>) : string[][] {
-
+    console.log("Compiling answer...")
     let solution = []
     Object.entries(all_accounts).forEach( entry => {
         let name = entry[0].split("_id")[0]
@@ -43,43 +93,39 @@ function compile_answer(all_accounts: Record<string, string[]>) : string[][] {
         emails = emails.filter( (email, i) => emails.indexOf(email) === i)
         emails = emails.sort()
 
-        console.log("Final Object: ", [name, ...emails])
         solution.push([name, ...emails])
 
     })
-
+    console.log("Answer:", solution)
     return solution
 }
 
 /**
- * Create a lookup table for account names and emails
+ * Scan our accounts lookup table, determining a status for the new entry.
+ * Return the key in which the emails will reside in our lookup table,
+ * and a boolean to determine if the key already exists (to merge emails later)
  */
 function verify_account(account: string[], all_accounts: Record<string, string[]>) : [boolean, string] {
 
     let account_name: string = account[0]
     let account_emails: string[] = account.slice(1)
     let all_verified = Object.entries(all_accounts) // [[name (our unique key), followed by emails], ...] just like the input
-    let is_new = true
-    let _key = account_name
+    
+    let is_new = true       // Return status
+    let _key = account_name // Return status
 
     // Look for an existing email in our object of organized (verified) accounts.
     all_verified.forEach( (entry) => {
-        // Compare emails between new entry and all existing entries
         if (account_emails.some( email => entry[1].includes(email) )) {
             is_new = false
             _key = entry[0]
-            console.log("Found a match!", account_name, "\nAt key: ", _key)
         }
     })
 
-    // console.log("Key: ", _key)
-    // console.log("is_new: ", is_new)
-    // console.log("account_name: ", account_name)
- 
     if (is_new) {
-        console.log("Creating new entry...")
+        
         /**
-         * Apply a unique key to this entry
+         * Create a unique key for the new entry.
          */
  
         let names = Object.keys(all_accounts)
@@ -89,12 +135,8 @@ function verify_account(account: string[], all_accounts: Record<string, string[]
             return name.split("_id")[0]
         })
 
-        console.log("All Names: ", all_names)
-
         // Count how many times this name has already appeared
         const n = all_names.filter( (name: string) => name === account_name ).length
-
-        console.log(`Found ${account_name} ${n} time(s).`)
 
         // Add a unique suffix and return the key
         is_new = true
